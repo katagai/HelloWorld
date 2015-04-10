ea47be579bf5d5825bbe2679e1744d08a54bfa90
g13913ks <g13913ks@s30454.tsuda.ac.jp>
2015-04-10 11:09:41 +0900

    HelloWorldです

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g13913ks
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        System.out.println("こんにちは世界");
+        System.out.println(10+5);
+    }
+}
